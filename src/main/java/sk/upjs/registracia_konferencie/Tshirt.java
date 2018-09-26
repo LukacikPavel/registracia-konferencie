939dafc8ab16b135b6f2f5ad32dc8326718d7ce9
student <student@158.197.35.83>
2018-09-26 09:35:29 +0200

    initial commit
    1. cvicenie

---
+++ src/main/java/sk/upjs/registracia_konferencie/Tshirt.java
@@ -0,0 +1,9 @@
+package sk.upjs.registracia_konferencie;
+
+public enum Tshirt {
+	S,
+	M,
+	L,
+	Xl,
+	XXL;
+}
---
+++ src/main/java/sk/upjs/registracia_konferencie/WorkShop.java
@@ -0,0 +1,14 @@
+package sk.upjs.registracia_konferencie;
+
+import java.time.LocalDate;
+
+public class WorkShop {
+	private String name;
+	private LocalDate start;
+	private LocalDate end;
+	private double priceFull;
+	private double priceStudent;
+	private double priceFullLate;
+	private double priceStudentLate;
+	
+}

f15cf46cc792b8021ea9cfc7a4c268bc918165e8
student <student@158.197.35.83>
2018-10-10 07:06:15 +0200

    3. cvicenie

--- src/main/java/sk/upjs/registracia_konferencie/Companion.java
+++ src/main/java/sk/upjs/registracia_konferencie/Companion.java
@@ -3,8 +3,62 @@ package sk.upjs.registracia_konferencie;
 import java.time.LocalDateTime;
 
 public class Companion {
+
+	public static final double[] COMPANION_FEES = new double[] { 60.0, 110.0, 165.0, 210.0 };
+	public static final double BANKET_FEE = 20.0;
+	public static final double TSHIRT_FEE = 5.0;
+	public static final LocalDateTime BANKET_DATETIME = LocalDateTime.of(2019, 9, 23, 19, 0);
+
 	private CompanionCategory category;
 	private LocalDateTime start;
 	private LocalDateTime end;
 	private Tshirt tshirt;
+
+	public double getPrice() {
+		double price = 0;
+		long nights = java.time.temporal.ChronoUnit.DAYS.between(start, end);
+		if (nights > 0) {
+			price = COMPANION_FEES[(int) nights];
+		}
+		if (start.isBefore(BANKET_DATETIME) && end.isAfter(BANKET_DATETIME)) {
+			price += BANKET_FEE;
+		}
+		if (tshirt != null) {
+			price += TSHIRT_FEE;
+		}
+		return price;
+
+	}
+
+	public CompanionCategory getCategory() {
+		return category;
+	}
+
+	public void setCategory(CompanionCategory category) {
+		this.category = category;
+	}
+
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getEnd() {
+		return end;
+	}
+
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+
+	public Tshirt getTshirt() {
+		return tshirt;
+	}
+
+	public void setTshirt(Tshirt tshirt) {
+		this.tshirt = tshirt;
+	}
 }
---
+++ src/test/java/sk/upjs/registracia_konferencie/CompanionTest.java
@@ -0,0 +1,32 @@
+package sk.upjs.registracia_konferencie;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+class CompanionTest {
+
+	
+	
+	@Test
+	void testGetPrice1() {
+		Companion c1 = new Companion();
+		c1.setCategory(CompanionCategory.ADULT);
+		c1.setStart(LocalDateTime.of(2019, 9, 21, 17, 0));
+		c1.setEnd(LocalDateTime.of(2019, 9, 23, 10, 0));
+		assertEquals(110, c1.getPrice());
+	}
+	
+	@Test
+	void testGetPrice2() {
+		Companion c1 = new Companion();
+		c1.setCategory(CompanionCategory.ADULT);
+		c1.setStart(LocalDateTime.of(2019, 9, 22, 17, 0));
+		c1.setEnd(LocalDateTime.of(2019, 9, 24, 10, 0));
+		c1.setTshirt(Tshirt.M);
+		assertEquals(135, c1.getPrice());
+	}
+	
+}
